730bf53c52241a7d35f4961078acd22f75bbe58f
razor-sharp <tarek.miski@gmail.com>
2012-11-11 15:28:03 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+	
+	$(".fundamentals").click(function(){
+		document.location.href='screens/fundamentals.html';
+	}).hover(function(){
+			$("#imgHolder img").fadeOut(function() { 
+			  $(this).load(function() { $(this).fadeIn(); }); 
+			  $(this).attr("src", "images/learn.jpg"); 
+			});
+		}, 
+		function(){
+			$("#imgHolder img").fadeOut(function() { 
+			  $(this).load(function() { $(this).fadeIn(); }); 
+			  $(this).attr("src", "images/caffeinated.png"); 
+			});
+		}
+	);
+	
+	$(".beginner").click(function(){
+		document.location.href='screens/beginner.html';
+	}).hover(
+		function(){
+			$("#imgHolder img").fadeOut(function() { 
+			  $(this).load(function() { $(this).fadeIn(); }); 
+			  $(this).attr("src", "images/beginner.jpg"); 
+			});
+		}, 
+		function(){
+			$("#imgHolder img").fadeOut(function() { 
+			  $(this).load(function() { $(this).fadeIn(); }); 
+			  $(this).attr("src", "images/caffeinated.png"); 
+			});
+		}
+	);
+		
+	$(".advanced").click(function(){
+		document.location.href='screens/advanced.html';
+	}).hover(
+		function(){
+			$("#imgHolder img").fadeOut(function() { 
+			  $(this).load(function() { $(this).fadeIn(); }); 
+			  $(this).attr("src", "images/advanced.jpg"); 
+			});
+		}, 
+		function(){
+			$("#imgHolder img").fadeOut(function() { 
+			  $(this).load(function() { $(this).fadeIn(); }); 
+			  $(this).attr("src", "images/caffeinated.png"); 
+			});
+		}
+	);
+	
+});
+
+/*
+$(window).load(function() {
+	$('.flexslider').flexslider({
+			start: function(slider){
+				$("#buttons button :eq(0)").addClass("highlight");
+			},
+			after: function(slider){
+				switch(slider.currentSlide){
+					case 0:
+						$("#buttons button :eq(0)").addClass("highlight");
+						$("#buttons button :eq(1)").removeClass("highlight");
+						$("#buttons button :eq(2)").removeClass("highlight");;
+						break;
+					case 1: 
+						$("#buttons button :eq(0)").removeClass("highlight");
+						$("#buttons button :eq(1)").addClass("highlight");
+						$("#buttons button :eq(2)").removeClass("highlight");
+						break;
+					case 2:
+						$("#buttons button :eq(0)").removeClass("highlight");
+						$("#buttons button :eq(1)").removeClass("highlight");
+						$("#buttons button :eq(2)").addClass("highlight");
+						break;
+					}
+			}
+	});
+});
+*/
